9f438f8e45f77d6dffc5a70c876ef12d2f7c9f42
valeraron <77055501+valeraron@users.noreply.github.com>
2021-10-10 17:12:18 +0300

    delete not importand file

--- src/components/App/App.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-export const Conteiner = styled.div`
-  padding: 20px 50px;
-  margin-left: auto;
-  margin-right: auto;
-`;
--- src/components/ContactList/ContactList.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const List = styled.ul`
-  padding: 10px;
-  margin: 0;
-`;
